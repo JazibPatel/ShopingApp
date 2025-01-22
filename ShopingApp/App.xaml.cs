caecef48df6a12afe3318df507b694a1be49e236
JazibPatel <jazibpatel@gmail.com>
2025-01-22 13:46:19 +0530

    hot fix

--- ShopingApp/App.xaml.cs
+++ ShopingApp/App.xaml.cs
@@ -7,6 +7,7 @@ namespace ShopingApp
         public App()
         {
             InitializeComponent();
+            
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
